e15d6921f40c43d1bc4f58832b820b03bead3ba6
DeyMass <silkin.mixail@yandex.ru>
2016-08-04 22:15:47 +0700

    Added range of attack

--- Assets/ShootTheEnemy.cs
+++ Assets/ShootTheEnemy.cs
@@ -1,17 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
 public class ShootTheEnemy : MonoBehaviour {
+    //public Material mat;
+    GameObject sphere;
+    Transform self;
+    float i = 0.0f;
+    bool MouseOn;
 
-    void Start() {
-        GameObject sphere = new GameObject();
-        sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        sphere.transform.position = new Vector3(15, 15, 10);
-        sphere.transform.localScale = new Vector3(10, 10, 1);
-        sphere.
+    void OnMouseOver()
+    {
+        MouseOn = true;
+        sphere.SetActive(true);
     }
 
-    void Update () {
-	
-	}
+    void OnMouseExit()
+    {
+        MouseOn = false;
+    }
+    
+    MeshRenderer VisibleSphere;
+    void Start()
+    {
+        self = GetComponent<Transform>();
+        sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        VisibleSphere = sphere.GetComponent<MeshRenderer>();
+        Shader shader = Shader.Find("Transparent/Diffuse");
+
+        VisibleSphere.material.shader = shader;
+        
+        sphere.transform.SetParent(self);
+        sphere.transform.position = new Vector3(self.transform.position.x, self.transform.position.y, self.transform.position.z + 1);
+        sphere.transform.localScale = new Vector3(20, 20, 0);
+        
+        sphere.SetActive(false);
+    }
+
+    void Update ()
+    {
+        if (MouseOn)
+            if (i < 1)
+                i = i + 0.05f;
+        if (!MouseOn)
+        {
+            if (i > 0)
+                i = i - 0.05f;
+            else
+                sphere.SetActive(false);
+        }
+        print(i);
+        if (VisibleSphere != null)
+            VisibleSphere.material.color = new Color(0, 0, 0, i);
+    }
 }
