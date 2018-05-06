67258602a2191e9436839602e9dd4051762a9552
DeyMass <silkin.mixail@yandex.ru>
2018-05-07 00:07:17 +0700

    Reworked game

---
+++ Assets/scripts/MyInterfaceHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyInterfaceHandler : MonoBehaviour {
+
+    public GameObject srcTower;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Keypad1)){
+            Instantiate<GameObject>(srcTower);
+        }
+	}
+}
---
+++ Assets/scripts/towerRadiusHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class towerRadiusHandler : MonoBehaviour {
+    private MeshRenderer render;
+    // Use this for initialization
+    void Start () {
+        render = transform.Find("RealRadius").gameObject.GetComponent<MeshRenderer>();
+        render.enabled = false;
+	}
+
+	void OnMouseOver()
+    {
+        if(render != null)
+            render.enabled = true;
+    }
+
+    void OnMouseExit()
+    {
+        render.enabled = false;
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
